f432bc1498304aee87d0c07eeac2908bbfecc049
LDT <ledientai16@gmail.com>
2024-01-11 22:21:04 +0700

    init project

---
+++ src/main/java/org/idk/instructorcrud/InstructorCrudApplication.java
@@ -0,0 +1,13 @@
+package org.idk.instructorcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstructorCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstructorCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/idk/instructorcrud/InstructorCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.idk.instructorcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstructorCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
